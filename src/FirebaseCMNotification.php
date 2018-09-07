b468df91563bd94f29ff2160906796c33eacdf9f
Vasyl Dmytruk <vasyl.dmytruk@xloo.com>
2018-09-07 17:10:53 +0300

    Added FirebaseCMNotification - Yii2 wrap on \autoxloo\fcm\FirebaseCloudMessaging, changed php version.

--- src/FirebaseCMNotification.php
+++ src/FirebaseCMNotification.php
@@ -2,15 +2,64 @@
 
 namespace autoxloo\yii2\fcm;
 
+use autoxloo\fcm\FirebaseCloudMessaging;
 use yii\base\Component;
+use yii\base\InvalidConfigException;
 
 /**
- * Class FirebaseCMNotification
+ * Class FirebaseCMNotification Yii2 wrap on [[\autoxloo\fcm\FirebaseCloudMessaging]].
+ * @see FirebaseCloudMessaging
  */
 class FirebaseCMNotification extends Component
 {
-    public static function test()
+    /**
+     * @var string Project ID of the Firebase project for your app.
+     * This ID is available in the General project settings tab of the Firebase console.
+     * @see https://console.firebase.google.com/project/_/settings/general/
+     */
+    public $projectId;
+    /**
+     * @var string Path to generated private key file in JSON format.
+     * @see https://console.firebase.google.com/project/_/settings/serviceaccounts/adminsdk
+     */
+    public $serviceAccountFile;
+
+    /**
+     * @var FirebaseCloudMessaging
+     */
+    protected $fcm;
+
+
+    /**
+     * @inheritdoc
+     * @throws InvalidConfigException
+     */
+    public function init()
     {
-        dump(__METHOD__);
+        parent::init();
+
+        if (empty($this->projectId)) {
+            throw new InvalidConfigException('Param "projectId" is required');
+        }
+
+        if (!file_exists($this->serviceAccountFile)) {
+            throw new InvalidConfigException(
+                "Param 'serviceAccountFile' ({$this->serviceAccountFile}) should be valid path to service account file"
+            );
+        }
+
+        $this->fcm = new FirebaseCloudMessaging($this->projectId, $this->serviceAccountFile);
+    }
+
+    /**
+     * Sends push notification.
+     *
+     * @param array $message Request body to send push notification.
+     *
+     * @return \GuzzleHttp\Psr7\Response
+     */
+    public function send($message)
+    {
+        return $this->fcm->send($message);
     }
 }
