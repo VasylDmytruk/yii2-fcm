1bb7ac9ecdcee2db567be7ab1c02c038da01c169
Vasyl Dmytruk <vasyl.dmytruk@xloo.com>
2018-09-07 16:51:34 +0300

    First commit

---
+++ src/FirebaseCMNotification.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace autoxloo\yii2\fcm;
+
+use yii\base\Component;
+
+/**
+ * Class FirebaseCMNotification
+ */
+class FirebaseCMNotification extends Component
+{
+    public static function test()
+    {
+        dump(__METHOD__);
+    }
+}
